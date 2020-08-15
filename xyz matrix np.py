1f9bd3d993fcf0e0f5a9ea20df4544c772283249
Nirma30 <nirma.m@aialz.com>
2020-08-15 17:25:04 +0530

    commit

---
+++ xyz matrix np.py
@@ -0,0 +1,11 @@
+import numpy as np
+x = np.array([[1,2],[3,4],[5,6],[7,8]])
+print(x)
+y = np.array([[1,2,3,4],[5,6,7,8]])
+print(y)
+z = np.array([[1,2],[3,4]])
+print(z)
+a = x.dot(y)
+print(a)
+b = y.dot(z)
+print(b)
