9c5a28d2d6da6b868ad5ed0b890560b1248bda77
Nirma30 <nirma.m@aialz.com>
2020-08-16 12:41:59 +0530

    commit

--- username.py
+++ username.py
@@ -1,15 +1,15 @@
 from tkinter import *
 a = Tk()
 a.title("LOGIN PAGE")
-# a.geometry("500x500+100+100")
-l1 = Label(text='LOGIN', fg='black', font=300).pack()
-l2 = Label(text='username', fg='black', font=100)
-l2.pack(side=LEFT)
-E1 = Entry(a, bd=5)
-E1.pack(side=RIGHT)
-l3 = Label(text='\n password', fg='black', font=100)
-l3.pack(anchor=SW)
-# l3.pack(after=l2, side=LEFT)
-E2 = Entry(a, bd=5)
-E2.pack(side=RIGHT)
+# a.geometry('500x500')
+a.configure(background = "grey")
+x = Label(a, text="username", font=20).grid(row=0,column=0)
+y = Label(a, text="password", font=20).grid(row=1, column=0)
+x1 = Entry(a).grid(row=0, column=1)
+y1 = Entry(a).grid(row=1, column=1)
+
+
+def clicked():
+    res = "welcome"
+bt = Button(a, text="submit").grid(row=3, column=0)
 a.mainloop()
